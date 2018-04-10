c85c7457f02b6ba1b079fafa709505c31b5cf3ed
yiwei <wangyiwei11259@gmail.com>
2018-04-10 18:49:26 +0800

    Add a table on "Read More" button click

---
+++ js/animate.js
@@ -0,0 +1,18 @@
+var text_description = document.getElementById('text-description');
+var table_description = document.getElementById('table-description');
+var read_more_btn = document.getElementById('read-more-btn');
+
+var count = 0;
+
+function change_div() {
+    if (count % 2 == 0) {
+        text_description.style.display = "none";
+        table_description.style.display = "block";
+        read_more_btn.firstChild.data = "Back";
+    } else {
+        table_description.style.display = "none";
+        text_description.style.display = "block";
+        read_more_btn.firstChild.data = "Read More";        
+    }
+    count++;
+}
